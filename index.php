973fa0f636ac934325f6b2f941e133981b31f517
Oleg <karelin71@gmail.com>
2021-02-11 13:36:05 +0300

    создал массив карточек с данными для постов

--- index.php
+++ index.php
@@ -1,7 +1,44 @@
 <?php
 $is_auth = rand(0, 1);
-
 $user_name = 'Oleg'; // укажите здесь ваше имя
+// добавляем массив карточек с данными для поста
+$posts = [
+    [
+    'title' => 'Цитата',
+    'type' => 'post-quote',
+    'content' => 'Мы в жизни любим только раз, а после ищем лишь похожих',
+    'author' => 'Лариса',
+    'avatar' => 'userpic-larisa-small.jpg'
+    ],
+    [
+    'title' => 'Игра престолов',
+    'type' => 'post-text',
+    'content' => 'Не могу дождаться начала финальногосезона своего любимого сериала!',
+    'author' => 'Владик',
+    'avatar' => 'userpic.jpg',
+    ],
+    [
+    'title' => 'Наконец, обработал фотки!',
+    'type' => 'post-photo',
+    'content' => 'rock-medium.jpg',
+    'author' => 'Виктор',
+    'avatar' => 'userpic-mark.jpg',
+    ],
+    [
+    'title' => 'Моя мечта',
+    'type' => 'post-photo',
+    'content' => 'coast-medium.jpg',
+    'author' => 'Лариса',
+    'avatar' => 'userpic-larisa-small.jpg'
+    ],
+    [
+    'title' => 'Лучшие курсы',
+    'type' => 'post-link',
+    'content' => 'www.htmlacademy.ru',
+    'author' => 'Владик',
+    'avatar' => 'userpic.jpg',
+    ],
+]; 
 ?>
 <!DOCTYPE html>
 <html lang="ru">
